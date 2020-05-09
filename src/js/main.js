dd468c691373a71ba738d7ed5123f2167e7e1e0c
stakansoljarki <57605002+stakansoljarki@users.noreply.github.com>
2020-05-09 12:29:43 +0300

    Feature: recipes page (#7)
    * add recipes page
    
    * add minor edits
    
    * add minor edit

--- src/js/main.js
+++ src/js/main.js
@@ -2,21 +2,9 @@ $(function () {
   burger();
   search();
   slick();
+  accordion();
 });
-let acc = document.getElementsByClassName("accordion");
 
-for (let i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    acc[i].classList.remove("active");
-    this.classList.add("active");
-    let panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
 if ($(window).width() < 1200) {
   const slider = document.querySelector(".header__menu");
   slider.remove();
